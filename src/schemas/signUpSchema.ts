ff43f680093234c61889a01ffa796595c6f3c77a
Akbar Ali <m.akberali507@gmail.com>
2026-02-27 22:03:18 +0500

    feat(auth): add sign-in and sign-up schemas for user validation

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,7 @@
+import z from "zod";
+
+export const signUpSchema = z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.email().min(1, "Email is required"),
+    password: z.string().min(1, "Password is required")
+})
